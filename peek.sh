60328e13fd4039cd0fe0461ba94452c782329665
Mario <mario.egea01@estudiant.upf.edu>
2023-11-02 13:17:04 +0100

    homework2

--- peek.sh
+++ peek.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 input_file=$1
 numberofrows=$2
-head -n $2 $input_file
-echo "..."
-tail -n $2 $input_file
+if [[ -z $numberofrows ]]; then numberofrows=3 ; fi
+
+if [[ $(wc -l < $1) -le $((2 * numberofrows)) ]] ; then cat $input_file ; fi
+
+if [[ $(wc -l < $1) -gt $((2 * numberofrows)) ]] ; then echo WARNING ; head -n $numberofrows $input_file ; echo "..." ; tail -n $numberofrows $input_file ; fi
